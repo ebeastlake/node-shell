2eec940ebbf7ddc0fd8017f84b6ca3a778c7bea3
ebeastlake <ebeastlake@gmail.com>
2017-07-31 11:48:47 -0400

    completed node bash

--- bash.js
+++ bash.js
@@ -1,17 +1,14 @@
 var commands = require('./command');
+var chalk = require('chalk'); 
 
-process.stdout.write('prompt > ');
+process.stdout.write(chalk.yellow('prompt > '));
 
 process.stdin.on('data', function(data) {
-	var userCommand = data.toString().trim();
+	var input = data.toString().trim().split(" "); 
+	var userCommand = input[0]; 
+	var userArg = input.slice(1).join(" ");
 
-	commands[userCommand]();
-
-	if (userCommand === 'date') {
-		var currDate = (new Date()).toString();
-		process.stdout.write(currDate);
-	}
-
-	process.stdout.write('\nprompt > ');
+	commands[userCommand](userArg);
+	process.stdout.write(chalk.yellow('\nprompt > '));
 
 })
--- command.js
+++ command.js
@@ -1,7 +1,21 @@
+var fs = require('fs'); 
+var chalk = require('chalk'); 
+
 module.exports = {
 	pwd: function() {
 		var currDir = process.env.PWD;
-		process.stdout.write(currDir);
+		process.stdout.write(chalk.green(currDir));
+	},
+	date: function() {
+		var currDate = (new Date()).toString(); 
+		process.stdout.write(chalk.green(currDate)); 
+	}, 
+	ls: function() {
+		process.stdout.write(chalk.green(fs.readdirSync('.').toString())); 
+	}, 
+	echo: function(userArg) { 
+		var echoData = (userArg[0] === '$' ? process.env[userArg.substring(1)] : userArg);
+		process.stdout.write(chalk.green(echoData));
 	}
 
 };
